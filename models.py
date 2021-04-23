3f13ab457bfd66e19397a626bdd51145aeea4913
whereislima <reach.lima@gmail.com>
2021-04-22 18:59:24 -0700

    first commit

---
+++ models.py
@@ -0,0 +1,12 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
